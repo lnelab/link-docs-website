dc72694609af22d6e1f45ced3f23ea7b4049744e
NWIron <497545532@qq.com>
2025-06-20 18:34:27 +0800

    update macros, releas v2501 for Ursa Major, Canis Major and Horizon75

--- components/Download.vue
+++ components/Download.vue
@@ -6,14 +6,19 @@ const BASE_URL = 'https://raw.githubusercontent.com/lnelab/dfu-package/main/'
 
 const items = [
     { name: 'Canis Major Left', fileName: 'sirius_cm_left_2402.zip' },
+    { name: 'Canis Major Left', fileName: 'sirius_cm_left_2501.zip' },
     { name: 'Canis Major Right', fileName: 'sirius_cm_right_2402.zip' },
+    { name: 'Canis Major Right', fileName: 'sirius_cm_right_2501.zip' },
     { name: 'LINKPAD 1U', fileName: 'sirius_linkpad_1u_2403.zip' },
     { name: 'LINKPAD', fileName: 'sirius_linkpad_2403.zip' },
     { name: 'MeowTool mini', fileName: 'sirius_meowtool_mini_2402.zip' },
     { name: 'MeowTool mini BLE', fileName: 'sirius_meowtool_mini_ble_2402.zip' },
     { name: 'Ursa Major Left', fileName: 'sirius_um_left_2402.zip' },
+    { name: 'Ursa Major Left', fileName: 'sirius_um_left_2501.zip' },
     { name: 'Ursa Major Right', fileName: 'sirius_um_right_2402.zip' },
-    { name: 'Horizon75', fileName: 'icon_horizon75_2402.zip' }
+    { name: 'Ursa Major Right', fileName: 'sirius_um_right_2501.zip' },
+    { name: 'Horizon75', fileName: 'icon_horizon75_2402.zip' },
+    { name: 'Horizon75', fileName: 'icon_horizon75_2501.zip' }
 ]
 </script>
 
