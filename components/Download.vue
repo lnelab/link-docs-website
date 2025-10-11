c025a08de9cec0b10770159c800ecbe05ebc808b
NWIron <497545532@qq.com>
2025-10-11 18:05:37 +0800

    update to 4.0.3

--- components/Download.vue
+++ components/Download.vue
@@ -4,33 +4,62 @@
 
 const BASE_URL = 'https://raw.githubusercontent.com/lnelab/dfu-package/main/'
 
-const items = [
-    { name: 'Canis Major Left', fileName: 'sirius_cm_left_2402.zip' },
-    { name: 'Canis Major Left', fileName: 'sirius_cm_left_2501.zip' },
-    { name: 'Canis Major Right', fileName: 'sirius_cm_right_2402.zip' },
-    { name: 'Canis Major Right', fileName: 'sirius_cm_right_2501.zip' },
-    { name: 'LINKPAD 1U', fileName: 'sirius_linkpad_1u_2403.zip' },
-    { name: 'LINKPAD 1U', fileName: 'sirius_linkpad_1u_2501.zip' },
-    { name: 'LINKPAD', fileName: 'sirius_linkpad_2403.zip' },
-    { name: 'LINKPAD', fileName: 'sirius_linkpad_2501.zip' },
-    { name: 'MeowTool mini', fileName: 'sirius_meowtool_mini_2402.zip' },
-    { name: 'MeowTool mini', fileName: 'sirius_meowtool_mini_2501.zip' },
-    { name: 'MeowTool mini BLE', fileName: 'sirius_meowtool_mini_ble_2402.zip' },
-    { name: 'MeowTool mini BLE', fileName: 'sirius_meowtool_mini_ble_2501.zip' },
-    { name: 'Ursa Major Left', fileName: 'sirius_um_left_2402.zip' },
-    { name: 'Ursa Major Left', fileName: 'sirius_um_left_2501.zip' },
-    { name: 'Ursa Major Right', fileName: 'sirius_um_right_2402.zip' },
-    { name: 'Ursa Major Right', fileName: 'sirius_um_right_2501.zip' },
-    { name: 'Horizon75', fileName: 'icon_horizon75_2402.zip' },
-    { name: 'Horizon75', fileName: 'icon_horizon75_2501.zip' }
+const deepItems = [
+    {
+        name: 'Canis Major',
+        files: [
+            'sirius_cm_left_2501.zip',
+            'sirius_cm_right_2501.zip'
+        ]
+    },
+    {
+        name: 'LINKPAD',
+        files: [
+            'sirius_linkpad_1u_2501.zip'
+        ]
+    },
+    {
+        name: 'LINKPAD 1U',
+        files: [
+            'sirius_linkpad_2501.zip'
+        ]
+    },
+    {
+        name: 'MeowTool mini',
+        files: [
+            'sirius_meowtool_mini_2501.zip'
+        ]
+    },
+    {
+        name: 'MeowTool mini BLE',
+        files: [
+            'sirius_meowtool_mini_ble_2501.zip'
+        ]
+    },
+    {
+        name: 'Ursa Major / 大熊座',
+        files: [
+            'sirius_um_left_2501.zip',
+            'sirius_um_right_2501.zip'
+        ]
+    },
+    {
+        name: 'Horizon75',
+        files: [
+            'icon_horizon75_2501.zip'
+        ]
+    }
 ]
 </script>
 
 <template>
-    <div>
+    <div v-for="{ name, files } in deepItems" :key="name">
+        <h3>
+            {{ name }}
+        </h3>
         <ul>
-            <li v-for="item in items" :key="item.fileName">
-                <a :href="BASE_URL + item.fileName">{{ item.name }} - {{ item.fileName }}</a>
+            <li v-for="file in files" :key="file">
+                <a :href="BASE_URL + file">{{ file }}</a>
             </li>
         </ul>
     </div>
